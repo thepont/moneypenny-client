1f6abee305f4c048d8b650d9288cb79e224b0503
Paul Esson <paul.esson@blueflag.com.au>
2016-01-12 12:01:14 +1100

    moneypenny-client changes

--- app/moneypenny-mongo-storage/stores/__test__/UserQueryTest.js
+++
@@ -1,7 +0,0 @@
-var UserQuery = require('../UserQuery');
-var QueryTests = require('moneypenny-mongo-storage/util/test/QueryTests');
-
-describe('UserQuery()', () => {   
-    QueryTests.itsAValidQueryObject(UserQuery);
-	QueryTests.itCreatesIDQuery(UserQuery);
-});
