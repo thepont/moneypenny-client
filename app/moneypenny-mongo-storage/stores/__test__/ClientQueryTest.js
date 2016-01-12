ee0309dce9216a5fde0e225e95c509a7151baa94
Paul Esson <paul.esson@blueflag.com.au>
2016-01-12 12:01:14 +1100

    moneypenny-client changes

--- app/moneypenny-mongo-storage/stores/__test__/ClientQueryTest.js
+++
@@ -1,8 +0,0 @@
-
-var OAuth2ClientQuery = require('../ClientQuery');
-var QueryTests = require('moneypenny-mongo-storage/util/test/QueryTests');
-
-describe('ClientQuery()', () => {   
-    QueryTests.itsAValidQueryObject(OAuth2ClientQuery);
-    QueryTests.itCreatesIDQuery(OAuth2ClientQuery);
-});
